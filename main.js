23c39463247ee6d951029d7a24c488037131ead0
Mark Stahl <markstahl@Marks-MacBook-Pro.local>
2018-02-05 19:08:13 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// define the isPangram() function
+let isPangram = (sentence) => {
+  let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let sentenceLowerCase = sentence.toLowerCase();
+  for (i = 0; i < alphabet.length; i++) {
+    return true;
+    if (!sentenceLowerCase.includes(alphabet[i])) {
+      return false;
+    }
+  }
+};
+// test the isPangram() function, and print results to the console.
+let testData = [
+  { sentence: 'The quick brown fox jumps over the lazy dog.', pangram: true },
+  { sentence: 'The quick onyx goblin jumps over the lazy dwarf.', pangram: true },
+  { sentence: 'Grumpy wizards make toxic brew for the evil queen and jack.', pangram: true },
+  { sentence: 'Not a pangram', pangram: false },
+  { sentence: '', pangram: false },
+]
+
+testData.forEach((trial) => {
+  let result = isPangram(trial.sentence)
+  let correct = (result == trial.pangram)
+  if (correct === true) {
+    console.log(`isPangram gave a correct result for: ${trial.sentence}`)
+  } else {
+    console.log(`isPangram gave an incorrect result for: ${trial.sentence}`)
+  }
+})
---
+++ pangram-js.md
@@ -0,0 +1,31 @@
+### Introduction
+
+A pangram is a sentence that uses every letter of the alphabet.
+
+> The quick brown fox jumps over the lazy dog.
+
+> The quick onyx goblin jumps over the lazy dwarf.
+
+> Grumpy wizards make toxic brew for the evil queen and jack.
+
+
+## Getting Started
+
+```no-highlight
+$ cd ~/challenges
+$ et get pangram-js
+$ cd pangram-js
+```
+
+Open the project folder in your code editor. In your `main.js` file, you'll see an array of test sentences, each of which has a `sentence` attribute (containing the text) and a `pangram` attribute (which is `true` or `false` based on whether the sentence is a pangram).
+
+### Instructions
+
+For each sentence in the array, we want to be able to call an `isPangram` function on it, which will return `true` if a given sentence is a pangram, and returns `false`, otherwise.
+
+We have provided most of the starter code in your `main.js` file. Please complete the `isPangram` function so it accomplishes our goals above!
+
+### External Resources
+
+* [Pangram - Wikipedia](https://en.wikipedia.org/wiki/Pangram)
+* [List of Pangrams](http://clagnut.com/blog/2380/)
